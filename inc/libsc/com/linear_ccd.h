ae3632703a869050080e54c04ff26c256a08a6f9
Ming Ming <mint2648@gmail.com>
2014-02-22 23:18:55 +0800

    Added Linear CCD support

--- inc/libsc/com/linear_ccd.h
+++ inc/libsc/com/linear_ccd.h
@@ -1,14 +1,39 @@
 /*
  * linear_ccd.h
+ * Linear CCD (for TSL1401CL)
  *
- * Author: Ming
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
  */
 
-#ifndef LINEAR_CCD_H_
-#define LINEAR_CCD_H_
+#ifndef LIBSC_LINEAR_CCD_H_
+#define LIBSC_LINEAR_CCD_H_
 
+#include <cstdint>
 
+namespace libsc
+{
 
+class LinearCcd
+{
+public:
+	static constexpr int SENSOR_W = 256;
 
+	LinearCcd();
 
-#endif /* LINEAR_CCD_H_ */
+	/**
+	 * Return the captured data in a bool array, where light pixel is true,
+	 * false otherwise. Must NOT delete the returned array
+	 *
+	 * @return
+	 */
+	const bool* SampleData();
+
+private:
+	bool m_buffer[SENSOR_W];
+	bool m_clk_state;
+};
+
+}
+
+#endif /* LIBSC_LINEAR_CCD_H_ */
---
+++ src/libsc/com/k60/linear_ccd.cpp
@@ -0,0 +1,56 @@
+/*
+ * linear_ccd.h
+ * Linear CCD (for TSL1401CL)
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ */
+
+#include <mini_common.h>
+#include <hw_common.h>
+#include <cstdint>
+
+#include <MK60_gpio.h>
+
+#include "libsc/com/config.h"
+#include "libsc/com/linear_ccd.h"
+
+namespace libsc
+{
+
+LinearCcd::LinearCcd()
+		: m_clk_state(true)
+{
+	// PTB10, AO(D1)
+	gpio_init(LIBSC_LINEAR_CCD0_AO, GPI, 1);
+	// PTB9 , Clock / CLK
+	gpio_init(LIBSC_LINEAR_CCD0_CLK, GPO, 1);
+	// PTB8 , SI
+	gpio_init(LIBSC_LINEAR_CCD0_SI, GPO, 1);
+}
+
+const bool* LinearCcd::SampleData()
+{
+	gpio_set(LIBSC_LINEAR_CCD0_SI, 1);
+	bool is_si_triggered = true;
+
+	for (int i = 0; i < SENSOR_W; ++i)
+	{
+		// gpio_turn is not working!
+		//gpio_turn(LIBSC_LINEAR_CCD0_CLK);
+		gpio_set(LIBSC_LINEAR_CCD0_CLK, m_clk_state);
+		m_clk_state ^= true;
+
+		// White == true
+		m_buffer[i] = (gpio_get(LIBSC_LINEAR_CCD0_AO) == 1);
+
+		if (is_si_triggered)
+		{
+			gpio_set(LIBSC_LINEAR_CCD0_SI, 0);
+			is_si_triggered = false;
+		}
+	}
+	return m_buffer;
+}
+
+}
