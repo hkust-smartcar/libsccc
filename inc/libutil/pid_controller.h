c17db6b3186ba377a06bc1199275365dc3600780
Ming Ming <mint2648@gmail.com>
2014-03-15 21:56:57 +0800

    Migrate PID controller to library

---
+++ inc/libutil/pid_controller.h
@@ -0,0 +1,61 @@
+/*
+ * pid_controller.h
+ * Generic PID controller
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ */
+
+#ifndef LIBUTIL_PID_CONTROLLER_H_
+#define LIBUTIL_PID_CONTROLLER_H_
+
+#include <cstdint>
+
+#include "libutil/clock.h"
+
+namespace libutil
+{
+
+class PidController
+{
+public:
+	PidController(const uint16_t setpoint, const float kp, const float ki,
+			const float kd);
+
+
+	uint16_t Calc(const Clock::ClockInt time, const uint16_t current_val);
+
+	void SetSetpoint(const uint16_t setpoint)
+	{
+		m_setpoint = setpoint;
+	}
+
+	void SetKp(const float kp)
+	{
+		m_kp = kp;
+	}
+
+	void SetKi(const float ki)
+	{
+		m_ki = ki;
+	}
+
+	void SetKd(const float kd)
+	{
+		m_kd = kd;
+	}
+
+private:
+	uint16_t m_setpoint;
+	float m_kp;
+	float m_ki;
+	float m_kd;
+
+	int32_t m_accumulated_error;
+	int16_t m_prev_error;
+	Clock::ClockInt m_prev_time;
+};
+
+}
+
+#endif /* LIBUTIL_PID_CONTROLLER_H_ */
---
+++ src/libutil/pid_controller.cpp
@@ -0,0 +1,41 @@
+/*
+ * pid_controller.cpp
+ * Generic PID controller
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ */
+
+#include <cstdint>
+
+#include <libutil/clock.h>
+
+#include "libutil/pid_controller.h"
+
+namespace libutil
+{
+
+PidController::PidController(const uint16_t setpoint, const float kp,
+		const float ki, const float kd)
+		: m_setpoint(setpoint), m_kp(kp), m_ki(ki), m_kd(kd),
+		  m_accumulated_error(0), m_prev_error(0), m_prev_time(Clock::Time())
+{}
+
+uint16_t PidController::Calc(const Clock::ClockInt time,
+		const uint16_t current_val)
+{
+	const Clock::ClockInt time_diff = Clock::TimeDiff(time, m_prev_time);
+	const uint32_t error = m_setpoint - current_val;
+
+	const float p = m_kp * error;
+	m_accumulated_error += error;
+	const float i = m_ki * m_accumulated_error;
+	const float slope = (error - m_prev_error) / (float)time_diff;
+	const float d = m_kd * slope;
+
+	m_prev_error = error;
+	m_prev_time = time;
+	return p + i + d;
+}
+
+}
