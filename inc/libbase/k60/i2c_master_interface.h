04b53b6fafd19219b410af7c165db48a05cb0964
Ming Ming <mint2648@gmail.com>
2014-09-16 12:58:52 +0800

    Add I2C Master interface

---
+++ inc/libbase/k60/i2c_master_interface.h
@@ -0,0 +1,47 @@
+/*
+ * i2c_master_interface.h
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+#include <vector>
+
+#include "libbase/k60/misc_utils.h"
+#include "libbase/k60/pin.h"
+
+namespace libbase
+{
+namespace k60
+{
+
+class I2cMasterInterface
+{
+public:
+	struct Config
+	{
+		Pin::Name scl_pin;
+		Pin::Name sda_pin;
+	};
+
+	virtual ~I2cMasterInterface()
+	{}
+
+	virtual operator bool() const = 0;
+
+	virtual bool GetByte(const Byte slave_addr, const Byte reg_addr, Byte *out_byte)  = 0;
+	virtual std::vector<Byte> GetBytes(const Byte slave_addr,
+			const Byte reg_addr, const uint8_t size) = 0;
+	virtual bool SendByte(const Byte slave_addr, const Byte reg_addr,
+			const Byte byte) = 0;
+	virtual bool SendBytes(const Byte slave_addr, const Byte reg_addr,
+			const std::vector<Byte> &bytes) = 0;
+};
+
+}
+}
