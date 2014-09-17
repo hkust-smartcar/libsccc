af716016d875ea264e800ddaf6c3fff082b54206
Ming Ming <mint2648@gmail.com>
2014-09-18 01:35:06 +0800

    Enrich SysTick so that it can also be used as a timer

--- inc/libbase/k60/sys_tick.h
+++ inc/libbase/k60/sys_tick.h
@@ -7,7 +7,12 @@
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
+
+#include <functional>
+
+#include "libbase/k60/misc_utils.h"
 
 #ifdef SysTick
 #undef SysTick
@@ -21,7 +26,46 @@ namespace k60
 class SysTick
 {
 public:
-	static void DelayUs(const uint16_t us);
+	typedef std::function<void(SysTick *sys_tick)> OnSysTickTriggerListener;
+
+	struct Config
+	{
+		uint32_t count;
+		OnSysTickTriggerListener isr;
+		bool is_enable = true;
+	};
+
+	explicit SysTick(const Config &config);
+	explicit SysTick(nullptr_t);
+	SysTick(const SysTick&) = delete;
+	SysTick(SysTick &&rhs);
+	~SysTick();
+
+	SysTick& operator=(const SysTick&) = delete;
+	SysTick& operator=(SysTick &&rhs);
+	operator bool() const
+	{
+		return m_is_init;
+	}
+
+	void SetEnable(const bool flag);
+
+	void SetCount(const uint32_t count);
+	uint32_t GetCountLeft() const;
+
+	void ConsumeInterrupt();
+	bool IsInterruptRequested() const;
+
+private:
+	void InitCsrReg(const Config &config);
+	void SetIsr(const OnSysTickTriggerListener &isr);
+	void Uninit();
+
+	static __ISR void IrqHandler();
+
+	OnSysTickTriggerListener m_isr;
+
+	bool m_is_init;
 };
 
 }
--- src/libbase/k60/sys_tick.cpp
+++ src/libbase/k60/sys_tick.cpp
@@ -8,45 +8,206 @@
 #include "libbase/k60/hardware.h"
 
 #include <cassert>
+#include <cstddef>
 #include <cstdint>
 
+#include <functional>
+
+#include "libbase/log.h"
 #include "libbase/k60/clock_utils.h"
 #include "libbase/k60/sys_tick.h"
+#include "libbase/k60/vectors.h"
 
-#define SysTickPtr ((SysTick_Type*)SysTick_BASE)
-#define SYST_CSR (SysTickPtr->CTRL)
-#define SYST_RVR (SysTickPtr->LOAD)
-#define SYST_CVR (SysTickPtr->VAL)
-#define SYST_CALIB (SysTickPtr->CALIB)
+#include "libutil/misc.h"
 
-#define SysTick_CSR_COUNTFLAG_SHIFT SysTick_CTRL_COUNTFLAG_Pos
-#define SysTick_CSR_CLKSOURCE_MASK SysTick_CTRL_CLKSOURCE_Msk
-#define SysTick_CSR_ENABLE_MASK SysTick_CTRL_ENABLE_Msk
-#define SysTick_CSR_ENABLE_SHIFT SysTick_CTRL_ENABLE_Pos
+#define SYST ((SysTick_Type*)SysTick_BASE)
+#define SYST_CSR (SYST->CTRL)
+#define SYST_RVR (SYST->LOAD)
+#define SYST_CVR (SYST->VAL)
+#define SYST_CALIB (SYST->CALIB)
+
+#define SYST_CSR_COUNTFLAG_SHIFT	SysTick_CTRL_COUNTFLAG_Pos
+#define SYST_CSR_CLKSOURCE_MASK		SysTick_CTRL_CLKSOURCE_Msk
+#define SYST_CSR_CLKSOURCE_SHIFT	SysTick_CTRL_CLKSOURCE_Pos
+#define SYST_CSR_TICKINT_MASK		SysTick_CTRL_TICKINT_Msk
+#define SYST_CSR_TICKINT_SHIFT		SysTick_CTRL_TICKINT_Pos
+#define SYST_CSR_ENABLE_MASK		SysTick_CTRL_ENABLE_Msk
+#define SYST_CSR_ENABLE_SHIFT		SysTick_CTRL_ENABLE_Pos
+
+#define SYST_RVR_RELOAD_MASK		SysTick_LOAD_RELOAD_Msk
+#define SYST_RVR_RELOAD_SHIFT		SysTick_LOAD_RELOAD_Pos
+#define SYST_RVR_RELOAD(x)			(((uint32_t)(((uint32_t)(x))<<SYST_RVR_RELOAD_SHIFT))&SYST_RVR_RELOAD_MASK)
+
+using namespace libutil;
 
 namespace libbase
 {
 namespace k60
 {
 
-void SysTick::DelayUs(const uint16_t us)
+namespace
 {
-	const uint32_t count = ClockUtils::GetCoreTickPerUs(us) - 1;
-	SYST_RVR = count & 0x00FFFFFFu;
-	SYST_CVR = 0;
-	SYST_CSR = 0 | SysTick_CSR_CLKSOURCE_MASK | SysTick_CSR_ENABLE_MASK;
-	while (!GET_BIT(SYST_CSR, SysTick_CSR_COUNTFLAG_SHIFT))
-	{}
 
-	if (count > 0x00FFFFFFu)
+inline void ConsumeInterrupt_()
+{
+	if (GET_BIT(SYST_CSR, SYST_CSR_COUNTFLAG_SHIFT))
 	{
-		SYST_RVR = count >> 24;
 		SYST_CVR = 0;
-		while (!GET_BIT(SYST_CSR, SysTick_CSR_COUNTFLAG_SHIFT))
-		{}
+	}
+}
+
+SysTick* g_instance = nullptr;
+
+}
+
+SysTick::SysTick(const Config &config)
+		: m_is_init(true)
+{
+	assert(!g_instance);
+	assert(config.count > 0);
+	g_instance = this;
+
+	SetEnable(false);
+	InitCsrReg(config);
+	SetCount(config.count);
+	ConsumeInterrupt();
+	SetIsr(config.isr);
+	if (config.is_enable)
+	{
+		SetEnable(true);
+	}
+}
+
+SysTick::SysTick(SysTick &&rhs)
+		: SysTick(nullptr)
+{
+	*this = std::move(rhs);
+}
+
+SysTick::SysTick(nullptr_t)
+		: m_is_init(false)
+{}
+
+SysTick::~SysTick()
+{
+	Uninit();
+}
+
+SysTick& SysTick::operator=(SysTick &&rhs)
+{
+	if (this != &rhs)
+	{
+		Uninit();
+		if (rhs)
+		{
+			rhs.m_is_init = false;
+
+			// Copy instead of move to prevent race condition
+			m_isr = rhs.m_isr;
+
+			m_is_init = true;
+			g_instance = this;
+		}
+	}
+	return *this;
+}
+
+void SysTick::InitCsrReg(const Config&)
+{
+	uint32_t reg = 0;
+	SET_BIT(reg, SYST_CSR_CLKSOURCE_SHIFT);
+
+	SYST_CSR = reg;
+}
+
+void SysTick::SetIsr(const OnSysTickTriggerListener &isr)
+{
+	m_isr = isr;
+	if (m_isr)
+	{
+		::SetIsr(SysTick_IRQn, IrqHandler);
+		SET_BIT(SYST_CSR, SYST_CSR_TICKINT_SHIFT);
+		//EnableIrq(SysTick_IRQn);
+	}
+	else
+	{
+		//DisableIrq(SysTick_IRQn);
+		::SetIsr(SysTick_IRQn, nullptr);
+		CLEAR_BIT(SYST_CSR, SYST_CSR_TICKINT_SHIFT);
+	}
+}
+
+void SysTick::Uninit()
+{
+	if (m_is_init)
+	{
+		m_is_init = false;
+
+		SetIsr(nullptr);
+		SetEnable(false);
+		g_instance = nullptr;
+	}
+}
+
+void SysTick::SetEnable(const bool flag)
+{
+	STATE_GUARD(SysTick, VOID);
+
+	if (flag)
+	{
+		SET_BIT(SYST_CSR, SYST_CSR_ENABLE_SHIFT);
+	}
+	else
+	{
+		CLEAR_BIT(SYST_CSR, SYST_CSR_ENABLE_SHIFT);
+	}
+}
+
+void SysTick::SetCount(const uint32_t count)
+{
+	STATE_GUARD(SysTick, VOID);
+
+	SYST_RVR = SYST_RVR_RELOAD(count);
+	SYST_CVR = 0;
+}
+
+uint32_t SysTick::GetCountLeft() const
+{
+	STATE_GUARD(SysTick, 0);
+
+	return SYST_CVR;
+}
+
+void SysTick::ConsumeInterrupt()
+{
+	STATE_GUARD(SysTick, VOID);
+
+	ConsumeInterrupt_();
+}
+
+bool SysTick::IsInterruptRequested() const
+{
+	STATE_GUARD(SysTick, false);
+
+	return GET_BIT(SYST_CSR, SYST_CSR_COUNTFLAG_SHIFT);
+}
+
+__ISR void SysTick::IrqHandler()
+{
+	SysTick *const that = g_instance;
+	if (!g_instance || !(*g_instance))
+	{
+		// Something's wrong?
+		assert(false);
+		ConsumeInterrupt_();
+		return;
 	}
 
-	CLEAR_BIT(SYST_CSR, SysTick_CSR_ENABLE_SHIFT);
+	if (that->m_isr)
+	{
+		that->m_isr(that);
+	}
+	ConsumeInterrupt_();
 }
 
 }
