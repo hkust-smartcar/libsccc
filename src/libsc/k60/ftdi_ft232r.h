fd2f05926b250fc6f6fa3a0936b29292df33b487
Ming Ming <mint2648@gmail.com>
2014-11-08 03:43:35 +0800

    Add FTDI FT232R

---
+++ src/libsc/k60/ftdi_ft232r.h
@@ -0,0 +1,22 @@
+/*
+ * ftdi_ft232r.h
+ * Generic class for the FTDI FT232R USB UART IC
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include "libsc/k60/uart_device.h"
+
+namespace libsc
+{
+namespace k60
+{
+
+typedef UartDevice FtdiFt232r;
+
+}
+}
