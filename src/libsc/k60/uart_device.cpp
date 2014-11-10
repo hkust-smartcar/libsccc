a2f8bd84bae381f18efc493a5d565d41abaf4455
Ming Ming <mint2648@gmail.com>
2014-11-11 06:32:50 +0800

    Fix null UartDevice

--- src/libsc/k60/uart_device.cpp
+++ src/libsc/k60/uart_device.cpp
@@ -353,7 +353,7 @@ void UartDevice::SendStr(string&&) {}
 void UartDevice::SendBuffer(const Byte*, const size_t) {}
 void UartDevice::SendBuffer(unique_ptr<Byte[]>&&, const size_t) {}
 void UartDevice::SendBuffer(vector<Byte>&&) {}
-void UartDevice::EnableRx(OnReceiveListener) {}
+void UartDevice::EnableRx(const OnReceiveListener&) {}
 void UartDevice::DisableRx() {}
 bool UartDevice::PeekChar(char*) { return false; }
 
