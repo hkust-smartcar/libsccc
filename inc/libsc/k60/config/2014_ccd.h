ee2ebceb1c8498266883169c85340a53219bd246
Ming Ming <mint2648@gmail.com>
2014-11-15 04:30:13 +0800

    Support RGB/BGR panel

--- inc/libsc/k60/config/2014_ccd.h
+++ inc/libsc/k60/config/2014_ccd.h
@@ -50,19 +50,12 @@
 #define LIBSC_JOYSTICK0_RIGHT libbase::k60::Pin::Name::kPtc7
 #define LIBSC_JOYSTICK0_SELECT libbase::k60::Pin::Name::kPtc9
 
-#ifdef LIBSC_USE_LCD_HW_SPI
-	#define LIBSC_LCD_RST libbase::k60::Pin::Name::kPta13
-	#define LIBSC_LCD_DC libbase::k60::Pin::Name::kPta17
-	#define LIBSC_LCD_CS libbase::k60::Pin::Name::kPta14
-	#define LIBSC_LCD_SDAT libbase::k60::Pin::Name::kPta16
-	#define LIBSC_LCD_SCLK libbase::k60::Pin::Name::kPta15
-#else
-	#define LIBSC_LCD_RST libbase::k60::Pin::Name::kPta13
-	#define LIBSC_LCD_DC libbase::k60::Pin::Name::kPta17
-	#define LIBSC_LCD_CS libbase::k60::Pin::Name::kPta14
-	#define LIBSC_LCD_SDAT libbase::k60::Pin::Name::kPta16
-	#define LIBSC_LCD_SCLK libbase::k60::Pin::Name::kPta15
-#endif
+#define LIBSC_LCD_RST libbase::k60::Pin::Name::kPta13
+#define LIBSC_LCD_DC libbase::k60::Pin::Name::kPta17
+#define LIBSC_LCD_CS libbase::k60::Pin::Name::kPta14
+#define LIBSC_LCD_SDAT libbase::k60::Pin::Name::kPta16
+#define LIBSC_LCD_SCLK libbase::k60::Pin::Name::kPta15
+#define LIBSC_ST7735R_BGR_PANEL 1
 
 #define LIBSC_LED0 libbase::k60::Pin::Name::kPtd0
 #define LIBSC_LED1 libbase::k60::Pin::Name::kPtd1
