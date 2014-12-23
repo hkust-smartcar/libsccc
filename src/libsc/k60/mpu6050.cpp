c822727ad8a10c31032747e8876448781831c148
Ming Ming <mint2648@gmail.com>
2014-12-23 23:18:07 +0800

    Trivial code clean up and optimization

--- src/libsc/k60/mpu6050.cpp
+++ src/libsc/k60/mpu6050.cpp
@@ -43,29 +43,31 @@ SoftI2cMaster::Config GetI2CConfig()
 
 }
 
-Mpu6050::Mpu6050():
-	m_i2c(GetI2CConfig())
+Mpu6050::Mpu6050()
+		: m_i2c(GetI2CConfig())
 {
-	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_PWR_MGMT_1,0x00);
+	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_PWR_MGMT_1, 0x00);
 
 	//Register 25 – Sample Rate Divider: Sample Rate = Gyroscope Output Rate / (1 + SMPLRT_DIV)
 	//Gyroscope Output Rate = 8kHz when the DLPF is disabled (DLPF_CFG = 0 or 7)
-	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_SMPLRT_DIV,0x00);
+	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_SMPLRT_DIV, 0x00);
 
 	//Register 26 - CONFIG: EXT_SYNC_SET[2:0]<<3 | DLPF_CFG[2:0];
 	//EXT_SYNC_SET=0, Input disabled;
 	//DLPF_CFG=0, Accel = 260Hz, Gyroscope = 256Hz;
-	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_CONFIG,0x00);
+	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_CONFIG, 0x00);
 
 	//Register 27 - GYRO_CONFIG: FS_SEL[1:0] << 3;
 	//FS_SEL=0, ± 250 °/s; FS_SEL=1, ± 500 °/s; FS_SEL=2, ± 1000 °/s; FS_SEL=3, ± 2000 °/s;
 	m_gyro_config = 2 << 3;
-	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_GYRO_CONFIG,m_gyro_config);
+	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_GYRO_CONFIG,
+			m_gyro_config);
 
 	//Register 28 - ACCEL_CONFIG: AFS_SEL[1:0] << 3;
 	//AFS_SEL=0, ±2g; AFS_SEL=1, ±4g; AFS_SEL=2, ±8g; AFS_SEL=3, ±16g;
 	m_accel_config = 2 << 3;
-	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_ACCEL_CONFIG,m_accel_config);
+	m_i2c.SendByte(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_ACCEL_CONFIG,
+			m_accel_config);
 }
 
 Mpu6050::~Mpu6050() {
@@ -121,23 +123,28 @@ float Mpu6050::GetAccelScaleFactor()
 	}
 }
 
-void Mpu6050::Update(){
-	vector<Byte> data = m_i2c.GetBytes(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_ACCEL_XOUT_H, 14);
-	int j;
-	for(int i = 0; i < data.size(); i+=2){
-		if(i>=0 && i<=5){
-			j = i/2;
-			m_raw_acc[j] = data[i] << 8 | data[i+1];
-			m_acc[j] = (float) m_raw_acc[j] * 1 / GetAccelScaleFactor();
+void Mpu6050::Update()
+{
+	const vector<Byte> &data = m_i2c.GetBytes(MPU6050_DEFAULT_ADDRESS,
+			MPU6050_RA_ACCEL_XOUT_H, 14);
+	for (size_t i = 0; i < data.size(); i += 2)
+	{
+		if (i >= 0 && i <= 5)
+		{
+			int j = i / 2;
+			m_raw_acc[j] = data[i] << 8 | data[i + 1];
+			m_acc[j] = (float) m_raw_acc[j] / GetAccelScaleFactor();
 		}
-		if(i==6){
-			m_raw_temp = data[i] << 8 | data[i+1];
+		else if (i == 6)
+		{
+			m_raw_temp = data[i] << 8 | data[i + 1];
 			m_temp = (float) m_raw_temp / 340 + 36.53;
 		}
-		if(i>=8 && i<=13){
-			j = (i - 8)/2;
-			m_raw_gyro[j] = data[i] << 8 | data[i+1];
-			m_omega[j] = (float) m_raw_gyro[j] * 1 / GetGyroScaleFactor();
+		else
+		{
+			int j = (i - 8) / 2;
+			m_raw_gyro[j] = data[i] << 8 | data[i + 1];
+			m_omega[j] = (float) m_raw_gyro[j] / GetGyroScaleFactor();
 		}
 	}
 }
@@ -152,7 +159,8 @@ const array<float, 3>& Mpu6050::GetOmega()
 	return m_omega;
 }
 
-float Mpu6050::GetCelsius(){
+float Mpu6050::GetCelsius()
+{
 	return m_temp;
 }
 
