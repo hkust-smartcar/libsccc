daa3f3841efc82eb6874e835c01a7b07bce4226b
Ming Ming <mint2648@gmail.com>
2015-02-09 16:17:05 +0800

    Adobt EndianUtils from my libutils

--- inc/libbase/misc_utils.h
+++ inc/libbase/misc_utils.h
@@ -12,14 +12,5 @@
 
 #include "libbase/misc_utils_c.h"
 
-// host to big-endian encoding
-uint32_t htobe32(const uint32_t host_32bits);
-// host to lil-endian encoding
-uint32_t htole32(const uint32_t host_32bits);
-// big-endian to host encoding
-uint32_t be32toh(const uint32_t big_endian_32bits);
-// lil-endian to host encoding
-uint32_t le32toh(const uint32_t little_endian_32bits);
-
 #define VOID
 #define STATE_GUARD(clz, ret) if (!(*this)) { LOG_WL(#clz " in illegal state"); return ret; }
