4f4ade8d0e9ea28adedb8d316d927030ec75a1d2
Ming Ming <mint2648@gmail.com>
2015-03-01 18:18:26 +0800

    Update config file for kl26z4dev

--- inc/libsc/kl26/config/vcan_z4dev.h
+++ inc/libsc/kl26/config/vcan_z4dev.h
@@ -1,18 +1,21 @@
 /*
  * vcan_z4dev.h
+ * Config file for the VCAN KL26 development board
  *
- *  Created on: Feb 15, 2015
- *      Author: harrison
+ * Author: Harrison Ng
+ * Copyright (c) 2014-2015 HKUST SmartCar Team
+ * Refer to LICENSE for details
  */
 
-#ifndef VCAN_Z4DEV_H_
-#define VCAN_Z4DEV_H_
+#pragma once
 
-#define LIBSC_USE_UART 1
+#define LIBSC_USE_LED 4
+//#define LIBSC_USE_UART 1
 
-#define LIBSC_UART0_TX libbase::kl26::Pin::Name::kPta14
-#define LIBSC_UART0_RX libbase::kl26::Pin::Name::kPta15
+#define LIBSC_LED0 libbase::kl26::Pin::Name::kPtd4
+#define LIBSC_LED1 libbase::kl26::Pin::Name::kPtd5
+#define LIBSC_LED2 libbase::kl26::Pin::Name::kPtd6
+#define LIBSC_LED3 libbase::kl26::Pin::Name::kPtd7
 
-
-
-#endif /* VCAN_Z4DEV_H_ */
+//#define LIBSC_UART0_TX libbase::kl26::Pin::Name::kPta14
+//#define LIBSC_UART0_RX libbase::kl26::Pin::Name::kPta15
