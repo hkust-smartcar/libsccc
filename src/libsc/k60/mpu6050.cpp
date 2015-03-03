78b1c4ac9681a58eb4339b3b431d1c7cf426edcf
Ming Ming <mint2648@gmail.com>
2015-03-04 01:21:44 +0800

    Fix broken null mpu6050

--- src/libsc/k60/mpu6050.cpp
+++ src/libsc/k60/mpu6050.cpp
@@ -192,7 +192,7 @@ Mpu6050::Mpu6050(const Config&)
 {
 	LOG_DL("Configured not to use Mpu6050");
 }
-bool Mpu6050::Update() { return false; }
+bool Mpu6050::Update(bool clamp_) { return false; }
 
 #endif /* LIBSC_USE_MPU6050 */
 
