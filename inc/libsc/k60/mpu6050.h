e5994b92baf96f07d48824c0b2ee02cce7df75e3
Harrison Ng <tplawyer@gmail.com>
2015-03-03 17:03:33 +0800

    Proper function overloading

--- inc/libsc/k60/mpu6050.h
+++ inc/libsc/k60/mpu6050.h
@@ -43,9 +43,7 @@ public:
 
 	explicit Mpu6050(const Config &config);
 
-	bool Update(bool clamp_);
-
-	bool Update();
+	bool Update(bool clamp_ = true);
 
 	const std::array<float, 3>& GetAccel()
 	{
