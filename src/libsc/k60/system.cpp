a11082c3819af78b5b2b2af8e0926cac909a64f6
Ming Ming <mint2648@gmail.com>
2015-03-04 13:43:49 +0800

    Enable cache automatically

--- src/libsc/k60/system.cpp
+++ src/libsc/k60/system.cpp
@@ -6,6 +6,8 @@
  * Refer to LICENSE for details
  */
 
+#include "libbase/k60/cache.h"
+
 #include "libsc/config.h"
 #include "libsc/k60/system.h"
 
@@ -21,7 +23,14 @@ System *System::m_instance = nullptr;
 System::System()
 		: m_delay(),
 		  m_timer()
-{}
+{
+	// Enable cache unless otherwise disabled
+#if !LIBSC_NOT_USE_CACHE && MK60F15
+	Cache::Config cache_conf;
+	cache_conf.is_enable = true;
+	Cache::Get().Init(cache_conf);
+#endif
+}
 
 }
 }
