1a6ddcb934128bf7b5af0af07066e282d98d8514
tselincoln <lktse@connect.ust.hk>
2015-06-01 21:04:33 +0800

    add new ccd board

---
+++ inc/libsc/k60/config/2015_ccd(2).h
@@ -0,0 +1,15 @@
+/*
+ * 2015_ccd(2).h
+ *
+ *  Created on: 1 Jun, 2015
+ *      Author: lincoln
+ */
+
+#ifndef INC_LIBSC_K60_CONFIG_2015_CCD_2__H_
+#define INC_LIBSC_K60_CONFIG_2015_CCD_2__H_
+
+
+
+
+
+#endif /* INC_LIBSC_K60_CONFIG_2015_CCD_2__H_ */
