3760ae70c03f27c5ac1ff2eb79e58ed282319225
Ming Ming <mint2648@gmail.com>
2015-06-12 17:42:49 +0800

    Tidy up code

--- src/libbase/kl26/spi_master.cpp
+++ src/libbase/kl26/spi_master.cpp
@@ -1,21 +1,46 @@
+/*
+ * spi_master.cpp
+ *
+ * Author: Harrison Ng
+ * Copyright (c) 2014-2015 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ */
+
 #include "libbase/kl26/hardware.h"
-#include "libbase/kl26/spi_master.h"
-#include "libbase/misc_utils_c.h"
-#include "libutil/misc.h"
-#include "libbase/kl26/sim.h"
-#include "libbase/kl26/pin.h"
-#include "libbase/kl26/clock_utils.h"
+
 #include <climits>
+#include <cstddef>
+#include <cstdint>
+
+#include <functional>
+
+#include "libbase/log.h"
+#include "libbase/kl26/clock_utils.h"
+#include "libbase/kl26/misc_utils.h"
+#include "libbase/kl26/pin.h"
+#include "libbase/kl26/pinout.h"
+#include "libbase/kl26/sim.h"
+#include "libbase/kl26/spi_master.h"
+
+#include "libutil/misc.h"
 
 using namespace libutil;
 
-namespace libbase {
-namespace kl26 {
+namespace libbase
+{
+namespace kl26
+{
 
-constexpr SPI_Type * MEM_MAPS[2] = { SPI0, SPI1 };
+namespace
+{
 
-SpiMaster::SpiMaster(const Config &config):
-	m_module(0){
+constexpr SPI_Type * MEM_MAPS[PINOUT::GetSpiCount()] = {SPI0, SPI1};
+
+}
+
+SpiMaster::SpiMaster(const Config &config)
+		: m_module(0)
+{
 //	Enable module clock
 	Sim::SetEnableClockGate(EnumAdvance(Sim::ClockGate::kSpi0,m_module), true);
 
@@ -95,31 +120,37 @@ SpiMaster::SpiMaster(const Config &config):
  * BaudRate = SPI Module Clock / BaudRateDivisor
  */
 	uint32_t module_clock = 0;
-	if(m_module == 0){
+	if (m_module == 0)
+	{
 		module_clock = ClockUtils::GetBusClock();
-	}else if(m_module == 1){
+	}
+	else if (m_module == 1)
+	{
 		module_clock = ClockUtils::GetCoreClock();
 	}
 
 	int diff = INT_MAX;
 	int best_sppr = 0;
 	int best_spr = 0;
-	for(int spr = 0; spr < 9; spr++ ){
-		if((module_clock/(2<<(spr))/(config.baud_rate_khz*1000)) <= 8){
+	for (int spr = 0; spr < 9; spr++)
+	{
+		if ((module_clock/(2<<(spr))/(config.baud_rate_khz*1000)) <= 8)
+		{
 			best_spr = spr;
 			break;
 		}
 	}
-	for(int sppr = 0; sppr <8; sppr++){
-
-			if(abs((module_clock/((sppr+1) * (2<<best_spr) )) - config.baud_rate_khz*1000) < diff){
-				best_sppr = sppr;
-				diff = abs((module_clock/((sppr+1) * (2<<best_spr) )) - config.baud_rate_khz*1000);
-				if(diff==0){
-					break;
-				}
+	for (int sppr = 0; sppr <8; sppr++)
+	{
+		if (abs((module_clock/((sppr+1) * (2<<best_spr) )) - config.baud_rate_khz*1000) < diff)
+		{
+			best_sppr = sppr;
+			diff = abs((module_clock/((sppr+1) * (2<<best_spr) )) - config.baud_rate_khz*1000);
+			if (diff==0)
+			{
+				break;
 			}
-
+		}
 	}
 	MEM_MAPS[m_module]->BR |= (SPI_BR_SPPR(best_sppr) | SPI_BR_SPR(best_spr));
 
@@ -127,11 +158,11 @@ SpiMaster::SpiMaster(const Config &config):
 	m_rx_isr = config.rx_isr;
 }
 
-SpiMaster::~SpiMaster() {
-	// TODO Auto-generated destructor stub
-}
+SpiMaster::~SpiMaster()
+{}
 
-uint16_t SpiMaster:: ExchangeData(const uint8_t, const uint16_t data){
+uint16_t SpiMaster:: ExchangeData(const uint8_t, const uint16_t data)
+{
 	uint16_t received;
 
 //	Wait untill SPTEF = 1
@@ -160,5 +191,15 @@ size_t SpiMaster::PushData(const uint8_t, const uint8_t*, const size_t)
 	return 0;
 }
 
-} /* namespace kl26 */
-} /* namespace libbase */
+void SpiMaster::SetEnableRxIrq(const bool)
+{
+	STATE_GUARD(SpiMaster, VOID);
+}
+
+void SpiMaster::SetEnableTxIrq(const bool)
+{
+	STATE_GUARD(SpiMaster, VOID);
+}
+
+}
+}
