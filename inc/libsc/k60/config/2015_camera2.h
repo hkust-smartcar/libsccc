caa18c54c1cdb1adc47fc65a35303a54859bdfc9
pklauac <laukiid@gmail.com>
2015-07-11 17:04:07 +0800

    update 2015 Camera2 joystick pin

--- inc/libsc/k60/config/2015_camera2.h
+++ inc/libsc/k60/config/2015_camera2.h
@@ -1,5 +1,3 @@
-
-
 #pragma once
 
 #define LIBSC_USE_BATTERY_METER 1
@@ -20,7 +18,7 @@
 #define LIBSC_BUTTON1 libbase::k60::Pin::Name::kPte7
 #define LIBSC_BUTTON2 libbase::k60::Pin::Name::kPta4
 
-#define LIBSC_BUZZER0 libbase::k60::Pin::Name::kPta7
+#define LIBSC_BUZZER0 libbase::k60::Pin::Name::kPta8
 //#define LIBSC_BUZZER_ACIVE_LEVEL 0
 
 #define LIBSC_ENCODER0_QDA libbase::k60::Pin::Name::kPta10
@@ -29,10 +27,10 @@
 #define LIBSC_ENCODER1_QDB libbase::k60::Pin::Name::kPta13
 
 #define LIBSC_JOYSTICK0_UP libbase::k60::Pin::Name::kPtc8
-#define LIBSC_JOYSTICK0_DOWN libbase::k60::Pin::Name::kPtc5
-#define LIBSC_JOYSTICK0_LEFT libbase::k60::Pin::Name::kPtc4
-#define LIBSC_JOYSTICK0_RIGHT libbase::k60::Pin::Name::kPtc6
-#define LIBSC_JOYSTICK0_SELECT libbase::k60::Pin::Name::kPtc7
+#define LIBSC_JOYSTICK0_DOWN libbase::k60::Pin::Name::kPtc4
+#define LIBSC_JOYSTICK0_LEFT libbase::k60::Pin::Name::kPtc5
+#define LIBSC_JOYSTICK0_RIGHT libbase::k60::Pin::Name::kPtc7
+#define LIBSC_JOYSTICK0_SELECT libbase::k60::Pin::Name::kPtc6
 
 #define LIBSC_ST7735R_RST libbase::k60::Pin::Name::kPte3
 #define LIBSC_ST7735R_DC libbase::k60::Pin::Name::kPte0
@@ -52,7 +50,7 @@
 
 #define LIBSC_MOTOR0_PWMA libbase::k60::Pin::Name::kPtd1
 #define LIBSC_MOTOR0_PWMB libbase::k60::Pin::Name::kPtd0
-#define LIBSC_MOTOR0_DEADTIME 1000
+#define LIBSC_MOTOR0_DEADTIME 2500
 
 
 #define LIBSC_MOTOR1_PWMA libbase::k60::Pin::Name::kPtd3
@@ -71,5 +69,3 @@
 
 #define LIBSC_UART0_TX libbase::k60::Pin::Name::kPte24
 #define LIBSC_UART0_RX libbase::k60::Pin::Name::kPte25
-
-
