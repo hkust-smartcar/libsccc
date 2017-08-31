640235b7e664ce500137be815269f82e443210b5
LeeChunHei <lch19980829@gmail.com>
2017-08-31 23:24:23 +0800

    2018 camera config modified

--- inc/libsc/k60/config/2018_camera.h
+++ inc/libsc/k60/config/2018_camera.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #define LIBSC_USE_BATTERY_METER 1
-#define LIBSC_USE_BUTTON 2
+#define LIBSC_USE_BUTTON 0
 #define LIBSC_USE_BUZZER 1
 #define LIBSC_USE_ENCODER 2
-#define LIBSC_USE_JOYSTICK 1
-#define LIBSC_USE_LCD 1
+#define LIBSC_USE_JOYSTICK 0
+#define LIBSC_USE_LCD 0
 #define LIBSC_USE_LED 4
 #define LIBSC_USE_MOTOR 2
 // #define LIBSC_USE_LINEAR_CCD 2
@@ -15,8 +15,8 @@
 
 #define LIBSC_BATTERY_METER libbase::k60::Adc::Name::kAdc3Ad6A
 
-#define LIBSC_BUTTON0 libbase::k60::Pin::Name::kPte7
-#define LIBSC_BUTTON1 libbase::k60::Pin::Name::kPtc12
+//#define LIBSC_BUTTON0 libbase::k60::Pin::Name::kPte7
+//#define LIBSC_BUTTON1 libbase::k60::Pin::Name::kPtc12
 
 #define LIBSC_BUZZER0 libbase::k60::Pin::Name::kPta8
 //#define LIBSC_BUZZER_ACIVE_LEVEL 0
@@ -26,17 +26,17 @@
 #define LIBSC_ENCODER1_QDA libbase::k60::Pin::Name::kPta12
 #define LIBSC_ENCODER1_QDB libbase::k60::Pin::Name::kPta13
 
-#define LIBSC_JOYSTICK0_UP libbase::k60::Pin::Name::kPtc8
-#define LIBSC_JOYSTICK0_DOWN libbase::k60::Pin::Name::kPtc4
-#define LIBSC_JOYSTICK0_LEFT libbase::k60::Pin::Name::kPtc5
-#define LIBSC_JOYSTICK0_RIGHT libbase::k60::Pin::Name::kPtc6
-#define LIBSC_JOYSTICK0_SELECT libbase::k60::Pin::Name::kPtc7
+//#define LIBSC_JOYSTICK0_UP libbase::k60::Pin::Name::kPtc8
+//#define LIBSC_JOYSTICK0_DOWN libbase::k60::Pin::Name::kPtc4
+//#define LIBSC_JOYSTICK0_LEFT libbase::k60::Pin::Name::kPtc5
+//#define LIBSC_JOYSTICK0_RIGHT libbase::k60::Pin::Name::kPtc6
+//#define LIBSC_JOYSTICK0_SELECT libbase::k60::Pin::Name::kPtc7
 
-#define LIBSC_ST7735R_RST libbase::k60::Pin::Name::kPte3
-#define LIBSC_ST7735R_DC libbase::k60::Pin::Name::kPte0
-#define LIBSC_ST7735R_CS libbase::k60::Pin::Name::kPte4
-#define LIBSC_ST7735R_SDAT libbase::k60::Pin::Name::kPte1
-#define LIBSC_ST7735R_SCLK libbase::k60::Pin::Name::kPte2
+//#define LIBSC_ST7735R_RST libbase::k60::Pin::Name::kPte3
+//#define LIBSC_ST7735R_DC libbase::k60::Pin::Name::kPte0
+//#define LIBSC_ST7735R_CS libbase::k60::Pin::Name::kPte4
+//#define LIBSC_ST7735R_SDAT libbase::k60::Pin::Name::kPte1
+//#define LIBSC_ST7735R_SCLK libbase::k60::Pin::Name::kPte2
 
 #define LIBSC_LED0 libbase::k60::Pin::Name::kPte12
 #define LIBSC_LED1 libbase::k60::Pin::Name::kPte11
@@ -48,12 +48,12 @@
 //#define LIBSC_MOTOR1_PWM libbase::k60::Pin::Name::kPtd2
 //#define LIBSC_MOTOR1_DIR libbase::k60::Pin::Name::kPtd3
 
-#define LIBSC_MOTOR0_PWMA libbase::k60::Pin::Name::kPtd0
-#define LIBSC_MOTOR0_PWMB libbase::k60::Pin::Name::kPtd1
+#define LIBSC_MOTOR0_PWMA libbase::k60::Pin::Name::kPte5
+#define LIBSC_MOTOR0_PWMB libbase::k60::Pin::Name::kPte6
 #define LIBSC_MOTOR0_DEADTIME 1000
 
-#define LIBSC_MOTOR1_PWMA libbase::k60::Pin::Name::kPtd2
-#define LIBSC_MOTOR1_PWMB libbase::k60::Pin::Name::kPtd3
+#define LIBSC_MOTOR1_PWMA libbase::k60::Pin::Name::kPte7
+#define LIBSC_MOTOR1_PWMB libbase::k60::Pin::Name::kPte8
 #define LIBSC_MOTOR1_DEADTIME 1000
 #define LIBSC_ALTERNATE_MOTOR_CW_PWM 1
 
@@ -64,11 +64,11 @@
 // #define LIBSC_LINEAR_CCD1_CLK libbase::k60::Pin::Name::kPtb2
 // #define LIBSC_LINEAR_CCD1_AD libbase::k60::Pin::Name::kPtb3
 
-#define LIBSC_OV77250_VSYNC libbase::k60::Pin::Name::kPtc2
-#define LIBSC_OV77250_PCLK libbase::k60::Pin::Name::kPtb6
-#define LIBSC_OV77250_DATA0 libbase::k60::Pin::Name::kPtb16
-#define LIBSC_OV77250_SDA libbase::k60::Pin::Name::kPtc1
-#define LIBSC_OV77250_SCL libbase::k60::Pin::Name::kPtc0
+#define LIBSC_OV77250_VSYNC libbase::k60::Pin::Name::kPta29
+#define LIBSC_OV77250_PCLK libbase::k60::Pin::Name::kPtb10
+#define LIBSC_OV77250_DATA0 libbase::k60::Pin::Name::kPtb0
+#define LIBSC_OV77250_SDA libbase::k60::Pin::Name::kPta25
+#define LIBSC_OV77250_SCL libbase::k60::Pin::Name::kPta26
 #define LIBSC_OV77250_DMA_CH 1
 
 #define LIBSC_SERVO0 libbase::k60::Pin::Name::kPta7
